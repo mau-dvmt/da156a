c9bdc77dc2dcac368b88f484469f76dfa90f9e39
Anton Tibblin <tibbelit@gmail.com>
2016-08-29 13:03:19 +0200

    Added lecture 1

---
+++ lectures/1/index.php
@@ -0,0 +1,14 @@
+<h2>Föreläsning 1 - Kursintroduktion</h2>
+
+<hr>
+
+<iframe src="//www.slideshare.net/slideshow/embed_code/key/wgR7aRylSabKPL" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen></iframe>
+<div style="margin-bottom:5px">
+    <strong>
+        <a href="//www.slideshare.net/AntonTibblin/ht16-da156a-kursintroduktion" title="HT16 - DA156A - Kursintroduktion" target="_blank">HT16 - DA156A - Kursintroduktion</a>
+    </strong>
+    from
+    <strong>
+        <a target="_blank" href="//www.slideshare.net/AntonTibblin">Anton Tibblin</a>
+    </strong>
+</div>
