fcadb3a9e5c002985518de530efbe2a19bbd765a
Anton Tibblin <tibbelit@gmail.com>
2016-09-27 16:49:03 +0200

    Info about exam

--- tenta.php
+++ tenta.php
@@ -4,7 +4,25 @@
         <h2>Om kursens tentamen</h2>
         <p>Förutom de praktiska inlämningsuppgifterna, så examineras kursen med en skriftlig tentamen. Den fångar upp många av de viktiga kunskaper och ställningstaganden man behöver känna till. Tentamen är också det moment som är VG-grundande för kursen.</p>
         <p>Skrivning sker under fyra timmar, första tillfället i slutet av kursens 10 veckor. Vid missad skrivning, eller icke godkänt betyg, finns flera möjligheter till omtentamen. Den första omtentan sker redan under höstterminen.</p>
-        <p><em>Mer information kring tentamen kommer publiceras när tentamen närmar sig.</em>
+        <h3>Exempeltentor</h3>
+        <p>För att visa hur uppgifterna ser ut är tre tidigare tentor publicerade. De är alla ganska lika till utformningen, och ger en bra indikation på kommande uppgifter. PDF-format:</p>
+        <ol>
+            <li><a href="/resources/examns/da156a-tenta-2011-10-12.pdf">2011-10-12</a>
+            </li>
+            <li><a href="/resources/examns/da156a-tenta-2011-12-02.pdf">2011-12-02</a>
+            </li>
+            <li><a href="/resources/examns/da156a-tenta-2012-02-29.pdf">2012-02-29</a>
+            </li>
+        </ol>
+
+        <h3>Att läsa</h3>
+        <p>Tentan sammanfattar kursens fokus, så god kunskap om innehåll i föreläsningar, laborationer och uppgifter är den bästa förberedelsen.</p>
+
+        <h3>Seminarium inför första tentamen</h3>
+        <p>Inför första skrivningen är ett seminarium inbokat, där frågor besvaras och möjliga lösningar kan diskuteras. Se <a href="/plan.php">planen</a>.</p>
+
+        <h3>Anmälan</h3>
+        <p>Anmälan till tentamen är <strong>obligatorisk</strong> och görs via <a href="https://minastudier.mah.se/">Mina studier</a>.</p>
 
     </div>
     <div class="col-lg-4 submenu-area">
