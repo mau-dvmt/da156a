f62d518ec194798b718dadcd73b37c669e845988
Tibbelit <tibbelit@gmail.com>
2019-09-11 15:37:40 +0200

    Added lecture 4

--- plan.php
+++ plan.php
@@ -46,7 +46,7 @@
                 <tr class="lecture odd">
                     <td>11/9</td>
                     <td>13-15</td>
-                    <td>Föreläsning 4: Introduktion till CSS</td>
+                    <td>Föreläsning 4: <a href="/lectures.php?nr=4">Introduktion till CSS</a></td>
 					<td>HTML &amp; CSS: Kap. 10-14</td>
                 </tr>
                 <tr class="laboration odd">
