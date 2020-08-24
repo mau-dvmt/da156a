551080d7c04e0894483804c77383e23d5ed522b1
Anton Tibblin <anton.tibblin@mau.se>
2020-08-24 11:34:18 +0200

    Typo

--- assignments.php
+++ assignments.php
@@ -21,7 +21,7 @@
 	<div class="row">
 		<div class="col-lg-8 create-submenu">
 			<h2>Inlämningsuppgifter</h2>
-			<p>Dessa uppgifter är, till skillnad från laborationerna, examinerande - vilket betyder att de är obligatoriska att genomföra för godkänt kursbetyg. Under höstterminen 2018 ingår tre uppgifter, som publiceras under kursens gång (enligt <a href="plan.php">planen</a>). Uppgifterna motsvarar 3,5 hp, och kan ge betyget Godkänd.</p>
+			<p>Dessa uppgifter är, till skillnad från laborationerna, examinerande - vilket betyder att de är obligatoriska att genomföra för godkänt kursbetyg. Under höstterminen 2020 ingår tre uppgifter, som publiceras under kursens gång (enligt <a href="plan.php">planen</a>). Uppgifterna motsvarar 3,5 hp, och kan ge betyget Godkänd.</p>
 			<p>Inlämningsuppgifterna publiceras under kursens gång.</p>
 			<table class="table table-striped">
 			    <tr>
