739ecff12d92321ce2fce7d4c71612bb7e50eaab
Anton Tibblin <tibbelit@gmail.com>
2024-09-05 10:16:51 +0200

    Added code to 2nd lecture

--- lectures/2/index.php
+++ lectures/2/index.php
@@ -8,39 +8,47 @@
 
 <hr>
 
-<!--
-
 <h3>Dagens exempel</h3>
 
-<p> Ni kan ladda ner hela webbplatsen i ZIP-format <a href="/lectures/2/f2.zip">här</a></p>
+<p> Ni kan ladda ner hela webbplatsen i ZIP-format <a href="/lectures/2/f2-h24.zip">här</a></p>
 
 <h4>Demo</h4>
 <h5>index.html</h5>
 <pre class="language-markup line-numbers" data-type="html"><code class="language-markup line-numbers">
-&lt;!doctype html&gt;
+&lt;!DOCTYPE html&gt;
 &lt;html lang=&quot;sv&quot;&gt;
-	&lt;head&gt;
-		&lt;meta charset=&quot;utf-8&quot;&gt;
-		&lt;title&gt;Min f&ouml;rsta webbsida!&lt;/title&gt;
-	&lt;/head&gt;
-	&lt;body&gt;
-		&lt;h1&gt;V&auml;lkommen till min webbsida!&lt;/h1&gt;
-		&lt;p&gt;Visst &auml;r den fin?&lt;/p&gt;
-		&lt;p&gt;Vill du se mina fina semesterbilder, &lt;a href=&quot;page.html&quot;&gt;klicka h&auml;r!&lt;/a&gt;&lt;/p&gt;
-		&lt;h2&gt;Filmrekommendationer&lt;/h2&gt;
-		&lt;ol&gt;
-			&lt;li&gt;Star Wars&lt;/li&gt;
-			&lt;li&gt;Oppenheimer&lt;/li&gt;
-			&lt;li&gt;Barbie&lt;/li&gt;
-			&lt;li&gt;Inception&lt;/li&gt;
-			&lt;li&gt;Matrix&lt;/li&gt;
-		&lt;/ol&gt;
-		&lt;h2&gt;Bra l&auml;nkar&lt;/h2&gt;
-		&lt;ul&gt;
-			&lt;li&gt;&lt;a href=&quot;http://mau.se&quot;&gt;Malm&ouml; universitet&lt;/a&gt;&lt;/li&gt;
-			&lt;li&gt;&lt;a href=&quot;http://google.com&quot;&gt;Google&lt;/a&gt;&lt;/li&gt;
-		&lt;/ul&gt;
-	&lt;/body&gt;
+    &lt;head&gt;
+        &lt;meta charset=&quot;utf-8&quot;&gt;
+        &lt;title&gt;Min f&ouml;rsta webbsida&lt;/title&gt;
+    &lt;/head&gt;
+
+    &lt;body&gt;
+        &lt;h1&gt;V&auml;lkommen till min fantastiska sida&lt;/h1&gt;
+        &lt;p&gt;H&auml;r &auml;r lite text... Vill du se mina semesterbilder &lt;a href=&quot;photos.html&quot;&gt;klicka h&auml;r!&lt;/a&gt;&lt;/p&gt;
+
+        &lt;h2&gt;Filmtips&lt;/h2&gt;
+        &lt;p&gt;H&auml;r kommer n&aring;gra filmtips:&lt;/p&gt;
+        &lt;ul&gt;
+            &lt;li&gt;Harry Potter&lt;/li&gt;
+            &lt;li&gt;Barbie&lt;/li&gt;
+            &lt;li&gt;Iron man&lt;/li&gt;
+            &lt;li&gt;Oppenheimer&lt;/li&gt;
+            &lt;li&gt;Top Gun&lt;/li&gt;
+        &lt;/ul&gt;
+
+        &lt;h2&gt;Bra l&auml;nkar&lt;/h2&gt;
+        &lt;ol&gt;
+            &lt;li&gt;
+                &lt;a href=&quot;https://mau.se&quot;&gt;Malm&ouml; Universitet&lt;/a&gt;
+            &lt;/li&gt;
+            &lt;li&gt;
+                &lt;a href=&quot;https://da106a.ia-mau.se&quot;&gt;Kurswebbplats&lt;/a&gt;
+            &lt;/li&gt;
+            &lt;li&gt;
+                &lt;a href=&quot;https://google.se&quot;&gt;Google&lt;/a&gt;
+            &lt;/li&gt;
+        &lt;/ol&gt;
+    &lt;/body&gt;
 &lt;/html&gt;
 </code></pre>
 
@@ -52,19 +60,16 @@
         &lt;meta charset=&quot;utf-8&quot;&gt;
         &lt;title&gt;Mina semesterbilder&lt;/title&gt;
     &lt;/head&gt;
+
     &lt;body&gt;
-        &lt;h1&gt;Semesterbilder&lt;/h1&gt;
-		&lt;p&gt;&lt;a href=&quot;index.html&quot;&gt;Klicka h&auml;r f&ouml;r att komma tillbaka till startsidan&lt;/a&gt;&lt;/p&gt;
-		
-		&lt;img src=&quot;images/Hobbiton.jpg&quot; alt=&quot;Anton leker hob&quot;&gt;
-		&lt;img src=&quot;images/Isthimus.jpg&quot; alt=&quot;Anton och en fin utsikt&quot;&gt;
-		&lt;img src=&quot;images/Kepler.jpg&quot; alt=&quot;Anton p&aring; hike&quot;&gt;
-		&lt;img src=&quot;images/StewartIsland.jpg&quot; alt=&quot;Anton vid b&auml;ck&quot;&gt;
-		
-		&lt;h2&gt;Inte en semesterbild&lt;/h2&gt;
-		&lt;img src=&quot;https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQTv0ezBtjKuvUDqu2E2zAy9wqdEshdMM6mPK6YAi407754oJa4UpyxoCOuJ1Wq0nOJNxbxdPUzkEH08mhe13YJB1SWVv00xr7tqIVPLFjT8UoK8nqLyCZ-&amp;usqp=CAc&quot; alt=&quot;Star Wars kaffemugg&quot;&gt;
+        &lt;h1&gt;Mina semesterbilder&lt;/h1&gt;
+        &lt;p&gt;&Auml;r du tr&ouml;tt p&aring; fina semesterbilder, &lt;a href=&quot;index.html&quot;&gt;klicka h&auml;r&lt;/a&gt; f&ouml;r att komma till startsidan&lt;/p&gt;
+        &lt;h2&gt;Bilder&lt;/h2&gt;
+        &lt;img src=&quot;images/Hobbiton.jpg&quot; alt=&quot;Anton leker att han &auml;r en hob&quot;&gt;
+        &lt;img src=&quot;images/Isthimus.jpg&quot; alt=&quot;Anton vandrar i berg&quot;&gt;
+        &lt;img src=&quot;images/Kepler.jpg&quot; alt=&quot;Anton vandrar i ett annat berg&quot;&gt;
+        &lt;h2&gt;Logotyper&lt;/h2&gt;
+        &lt;img src=&quot;https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg&quot; alt=&quot;Star Wars-logo&quot;&gt;
     &lt;/body&gt;
 &lt;/html&gt;
 </code></pre>
-
--->
